33ba25e679e60b2ed7774e13e880f0d9e82ddc6b
Timi Petre <gaxxis@yahoo.com>
2022-01-11 15:12:40 +0200

    Completed

---
+++ android/app/src/main/kotlin/com/timipetre/bitcointicker/bitcoin_ticker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.timipetre.bitcointicker.bitcoin_ticker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() {
+}
---
+++ lib/coin_data.dart
@@ -0,0 +1,31 @@
+const List<String> currenciesList = <String>[
+  'AUD',
+  'BRL',
+  'CAD',
+  'CNY',
+  'EUR',
+  'GBP',
+  'HKD',
+  'IDR',
+  'ILS',
+  'INR',
+  'JPY',
+  'MXN',
+  'NOK',
+  'NZD',
+  'PLN',
+  'RON',
+  'RUB',
+  'SEK',
+  'SGD',
+  'USD',
+  'ZAR'
+];
+
+const List<String> cryptoList = <String>[
+  'BTC',
+  'ETH',
+  'LTC',
+];
+
+class CoinData {}
